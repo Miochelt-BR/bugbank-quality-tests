a8ae5e06dea7149e9f4d18e3e824780ae6575a5c
Miochelt-BR <thiago.m.a@outlook.com>
2025-11-09 22:59:52 -0300

    chore: ajuste arquivo de visualização

--- cypress/e2e/acess.cy.js
+++ cypress/e2e/acess.cy.js
@@ -2,8 +2,6 @@ describe('Fluxo completo de cadastro', () => {
   it('acesso no site', () => {
     cy.visit('https://bugbank.netlify.app/')
   })
-  it('registro com sucesso',()=>{
+  
     
-
-  })
 })
